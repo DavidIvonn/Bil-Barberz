e8b2ae2a26a1be26693a5578c1d91fb8b25a072c
DavidIvonn <david.doraivonn-2018@keri.mako.hu>
2023-01-30 11:26:52 +0100

    Ivonn

---
+++ js/app.js
@@ -0,0 +1,46 @@
+;(function(window, angular) {
+
+  'use strict';
+
+  // Application module
+  angular.module('app', ['ui.router', 'app.common'])
+
+  /* Application config */
+  .config([
+    '$stateProvider', 
+    '$urlRouterProvider', 
+    function($stateProvider, $urlRouterProvider) {
+
+      $stateProvider
+        .state('home', {
+          url: '/',
+          templateUrl: './html/home.html'
+        })
+        .state('blog', {
+          url: '/blog',
+          templateUrl: './html/blog.html'
+        })
+        .state('contact', {
+          url: '/contact',
+          templateUrl: './html/contact.html'
+        })
+        .state('ar', {
+          url: '/ar',
+          templateUrl: './html/ar.html'
+        })
+        .state('termek', {
+          url: '/termek',
+          templateUrl: './html/termek.html'
+        })
+        .state('nyeremeny', {
+          url: '/nyeremeny',
+          templateUrl: './html/nyeremeny.html'
+        })
+        ;
+      
+      $urlRouterProvider.otherwise('/');
+    }
+  ])
+
+})(window, angular);
+
