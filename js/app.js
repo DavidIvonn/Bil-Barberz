232ee39050ec5959f985d322b2086a8d6b63a40d
Konyhasneni22 <balla.istvan-2018@keri.mako.hu>
2023-03-01 14:33:26 +0100

    Modified

--- js/app.js
+++ js/app.js
@@ -14,10 +14,7 @@
       $stateProvider
         .state('home', {
           url: '/',
-          templateUrl: './html/home.html',
-          styleUrls: ['./css/app.css'],
-          controller:'arkontroller'
-          
+          templateUrl: './html/home.html'
         })
         .state('blog', {
           url: '/blog',
@@ -30,7 +27,7 @@
         .state('ar', {
           url: '/ar',
           templateUrl: './html/ar.html',
-          controller:'arkontroller'
+          controller:"arkontroller"
 
         })
         .state('termek', {
@@ -61,87 +58,12 @@
             isAssoc: true, 
           },
         })
-        .then((data) => {
-          $scope.order = data; 
-          $scope.$applyAsync(); 
-          $scope.filter = null; 
-          $scope.orderFilter = (event) => {
-            let element = event.currentTarget;
-            $scope.filter = element.id; 
-            $scope.$applyAsync(); 
-          };
+        .then(data => {
+          $scope.data = data;
+          $scope.$applyAsync();
         })
         .catch((e) => console.log(e)); 
     },
   ]);
   
-});
-
-
-
-// AJAX kérés küldése az adatok lekéréséhez
-  var xhr = new XMLHttpRequest();
-  xhr.onreadystatechange = function() {
-    if (xhr.readyState === 4 && xhr.status === 200) {
-      // A JSON adatok parse-olása
-      var stilusok = JSON.parse(xhr.responseText);
-
-      // A termékek listájának lekérése a HTML-ből
-      var stilusList = document.getElementById('stilus-list');
-
-      // A termékek hozzáadása a HTML sablonhoz
-      stilusok.forEach(function(stilus) {
-        var card = document.createElement('div');
-        card.className = 'col-sm-6 col-md-4 col-lg-3';
-        card.innerHTML = `
-        <div class="card">
-          <img src="${stilus.kep}" class="card-img-top" alt="${stilus.ar}">
-          <div class="card-body">
-            <h5 class="card-title">${stilus.megnev}</h5>
-            <p class="card-text">${stilus.leiras}</p>
-            <a href="#" class="btn btn-primary">Buy now</a>
-          </div>
-        </div>
-      `;
-      stilusList.appendChild(card);
-    });
-  }
-};
-xhr.open('GET', 'arlista.php', true);
-xhr.send();
-
-
-       
-const showOnPx = 100;
-const backToTopButton = document.querySelector(".back-to-top");
-const pageProgressBar = document.querySelector(".progress-bar");
-
-const scrollContainer = () => {
-  return document.documentElement || document.body;
-};
-
-const goToTop = () => {
-  document.body.scrollIntoView({
-    behavior: "smooth"
-  });
-};
-
-document.addEventListener("scroll", () => {
-  console.log("Scroll Height: ", scrollContainer().scrollHeight);
-  console.log("Client Height: ", scrollContainer().clientHeight);
-
-  const scrolledPercentage =
-    (scrollContainer().scrollTop /
-      (scrollContainer().scrollHeight - scrollContainer().clientHeight)) *
-    100;
-
-  pageProgressBar.style.width = `${scrolledPercentage}%`;
-
-  if (scrollContainer().scrollTop > showOnPx) {
-    backToTopButton.classList.remove("hidden");
-  } else {
-    backToTopButton.classList.add("hidden");
-  }
-});
-
-backToTopButton.addEventListener("click", goToTop);
+})(window, angular);
